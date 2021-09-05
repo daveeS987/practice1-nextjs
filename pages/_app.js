c119e9be42bf79dd552c3e14d450bae42910c647
Davee Sok <daveesok@gmail.com>
2021-09-05 17:16:17 -0500

    changed default colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
 import '../styles/globals.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#06a05d',
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <CssBaseline />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }

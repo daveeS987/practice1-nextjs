07c9f806be3150dc2f9443ed4f832bbbb40107c4
Davee Sok <daveesok@gmail.com>
2021-09-06 22:21:37 -0500

    took out linter warnings

--- pages/detail/[itemId].js
+++ pages/detail/[itemId].js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { If, Then, Else, When, Unless } from 'react-if';
